38a861ee7d3e25f9be824b12f1148f55562e89c1
wc kim <wckim@smilelab.co.kr>
2025-01-22 20:55:28 +0900

    volumeCredit 계산을 별도 메서드로 분리

--- src/main/java/refactoringjavastudy/chapter01/Statement.java
+++ src/main/java/refactoringjavastudy/chapter01/Statement.java
@@ -10,13 +10,7 @@ public class Statement {
         int volumeCredit = 0;
         StringBuilder result = new StringBuilder(String.format("청구내역 (고객명: %s)\n", invoice.getCustomer()));
         for (Performance performance : invoice.getPerformances()) {
-            // 포인트를 적립한다.
-            volumeCredit += Math.max(performance.getAudience() - 30, 0);
-
-            // 희극 관객 5명마다 추가 포인트를 제공핟나.
-            if (playFor(plays, performance).getType().equals(PlayType.COMEDY)) {
-                volumeCredit += Math.floor(performance.getAudience() / 5);
-            }
+            volumeCredit += volumeCreditFor(plays, performance);
 
             // 청구 내역을 출력한다.
             result.append(String.format("%s: $%d %d석\n",playFor(plays, performance).getName(), amountFor(performance, plays) / 100, performance.getAudience()));
@@ -26,6 +20,19 @@ public class Statement {
         result.append(String.format("총액: $%d\n",totalAmount / 100));
         result.append(String.format("적립 포인트: %d점", volumeCredit));
         return result.toString();
+    }
+
+    private int volumeCreditFor(Plays plays, Performance performance) {
+        int result = 0;
+
+        // 포인트를 적립한다.
+        result += Math.max(performance.getAudience() - 30, 0);
+
+        // 희극 관객 5명마다 추가 포인트를 제공핟나.
+        if (playFor(plays, performance).getType().equals(PlayType.COMEDY)) {
+            result += Math.floor(performance.getAudience() / 5);
+        }
+        return result;
     }
 
     private int amountFor(Performance performance, Plays plays) throws Exception {
