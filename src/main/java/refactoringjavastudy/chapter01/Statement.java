210f9c1758663aa7459575011d56423aa87d4233
wc kim <wckim@smilelab.co.kr>
2025-01-22 21:10:07 +0900

    totalAmount도 volumeCredit과 마찬가지로 리팩토링 적용

--- src/main/java/refactoringjavastudy/chapter01/Statement.java
+++ src/main/java/refactoringjavastudy/chapter01/Statement.java
@@ -6,18 +6,23 @@ public class Statement {
      * 공연료 청구서를 출력하는 코드
      */
     public String statement(Invoice invoice, Plays plays) throws Exception {
-        int totalAmount = 0;
-
         StringBuilder result = new StringBuilder(String.format("청구내역 (고객명: %s)\n", invoice.getCustomer()));
         for (Performance performance : invoice.getPerformances()) {
             // 청구 내역을 출력한다.
             result.append(String.format("%s: $%d %d석\n",playFor(plays, performance).getName(), amountFor(performance, plays) / 100, performance.getAudience()));
-            totalAmount += amountFor(performance, plays);
         }
 
-        result.append(String.format("총액: $%d\n",totalAmount / 100));
+        result.append(String.format("총액: $%d\n",totalAmount(invoice, plays)));
         result.append(String.format("적립 포인트: %d점", totalVolumeCredits(invoice, plays)));
         return result.toString();
+    }
+
+    private int totalAmount(Invoice invoice, Plays plays) throws Exception {
+        int totalAmount = 0;
+        for (Performance performance : invoice.getPerformances()) {
+            totalAmount += amountFor(performance, plays);
+        }
+        return totalAmount / 100;
     }
 
     private int totalVolumeCredits(Invoice invoice, Plays plays) {
