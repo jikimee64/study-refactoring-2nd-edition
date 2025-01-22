3589a9ee57f7e0e6b8bb3b7552b6365cfbd7d134
wc kim <wckim@smilelab.co.kr>
2025-01-22 19:12:58 +0900

    git init

---
+++ src/main/java/refactoringjavastudy/RefactoringJavaStudyApplication.java
@@ -0,0 +1,13 @@
+package refactoringjavastudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefactoringJavaStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RefactoringJavaStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/refactoringjavastudy/RefactoringJavaStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package refactoringjavastudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefactoringJavaStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
