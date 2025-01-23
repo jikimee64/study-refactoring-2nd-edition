34232e3ca7664b4f58dff3da0fc884a5c378916f
wc kim <wckim@smilelab.co.kr>
2025-01-23 22:45:41 +0900

    계산 단계인 StatementData 생성 및 포맷팅 단계인 Statement 분리

--- src/main/java/refactoringjavastudy/chapter01/Statement.java
+++ src/main/java/refactoringjavastudy/chapter01/Statement.java
@@ -6,74 +6,34 @@ public class Statement {
      * 공연료 청구서를 출력하는 코드
      */
     public String statement(Invoice invoice, Plays plays) throws Exception {
-        return renderPlainText(invoice, plays);
+        StatementData statementData = new StatementData(invoice, plays);
+        return renderPlainText(statementData);
     }
 
-    private String renderPlainText(Invoice invoice, Plays plays) throws Exception {
-        StringBuilder result = new StringBuilder(String.format("청구내역 (고객명: %s)\n", invoice.getCustomer()));
-        for (Performance performance : invoice.getPerformances()) {
+    private String renderPlainText(StatementData statementData) throws Exception {
+        StringBuilder result = new StringBuilder(String.format("청구내역 (고객명: %s)\n", statementData.getCustomer()));
+        for (Performance performance : statementData.getPerformances()) {
             // 청구 내역을 출력한다.
-            result.append(String.format("%s: $%d %d석\n",playFor(plays, performance).getName(), amountFor(performance, plays) / 100, performance.getAudience()));
+            result.append(String.format("%s: $%d %d석\n", statementData.playFor(performance).getName(), statementData.amountFor(performance) / 100, performance.getAudience()));
         }
 
-        result.append(String.format("총액: $%d\n",totalAmount(invoice, plays)));
-        result.append(String.format("적립 포인트: %d점", totalVolumeCredits(invoice, plays)));
+        result.append(String.format("총액: $%d\n",statementData.totalAmount()));
+        result.append(String.format("적립 포인트: %d점", statementData.totalVolumeCredits()));
         return result.toString();
     }
 
-    private int totalAmount(Invoice invoice, Plays plays) throws Exception {
-        int totalAmount = 0;
-        for (Performance performance : invoice.getPerformances()) {
-            totalAmount += amountFor(performance, plays);
+    private String renderHtml(StatementData statementData) throws Exception {
+        StringBuilder result = new StringBuilder(String.format("<h1> 청구내역 (고객명: %s)\n </h1>", statementData.getCustomer()));
+        result.append("<table> \n");
+        result.append("<tr><th> 연극 </th> <th>좌석 수</th> <th>금액</th>");
+        for (Performance performance : statementData.getPerformances()) {
+            result.append(String.format("<tr><td> %s: </td> <td> $%d </td> <td> %d석 </td></tr>\n",statementData.playFor(performance).getName(), statementData.amountFor(performance) / 100, performance.getAudience()));
         }
-        return totalAmount / 100;
-    }
+        result.append("</table>\n");
 
-    private int totalVolumeCredits(Invoice invoice, Plays plays) {
-        int volumeCredit = 0;
-        for (Performance performance : invoice.getPerformances()) {
-            volumeCredit += volumeCreditFor(plays, performance);
-        }
-        return volumeCredit;
-    }
-
-    private int volumeCreditFor(Plays plays, Performance performance) {
-        int result = 0;
-
-        // 포인트를 적립한다.
-        result += Math.max(performance.getAudience() - 30, 0);
-
-        // 희극 관객 5명마다 추가 포인트를 제공핟나.
-        if (playFor(plays, performance).getType().equals(PlayType.COMEDY)) {
-            result += Math.floor(performance.getAudience() / 5);
-        }
-        return result;
-    }
-
-    private int amountFor(Performance performance, Plays plays) throws Exception {
-        int result;
-        switch (playFor(plays, performance).getType()) {
-            case TRAGEDY:
-                result = 40000;
-                if (performance.getAudience() > 30) {
-                    result += 1000 * (performance.getAudience() - 30);
-                }
-                break;
-            case COMEDY:
-                result = 30000;
-                if (performance.getAudience() > 20) {
-                    result += 10000 + 500 * (performance.getAudience() - 20);
-                }
-                result += 300 * performance.getAudience();
-                break;
-            default:
-                throw new Exception("알 수 없는 장르");
-        }
-        return result;
-    }
-
-    private Play playFor(Plays plays, Performance performance) {
-        return plays.get(performance);
+        result.append(String.format("총액: $%d\n", statementData.totalAmount()));
+        result.append(String.format("적립 포인트: %d점", statementData.totalVolumeCredits()));
+        return result.toString();
     }
 
 }
---
+++ src/test/java/refactoringjavastudy/StatementDataTest.java
@@ -0,0 +1,5 @@
+package refactoringjavastudy;
+
+public class StatementDataTest {
+
+}
